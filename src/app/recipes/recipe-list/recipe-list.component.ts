a7e42e4eca8ef72753dd6ea86f8294307c0b2124
farooq-outplay <farooq.m@outplayhq.com>
2022-08-14 11:01:18 +0530

    creating Recipe Model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  recipes = [];
 
   constructor() { }
 
